c9bd3cfc3bec6b634e48ba3c530105b1d6af61ef
Hildering,Raymond <i421449@student.fontys.nl>
2019-11-27 15:59:30 +0100

    start

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
+function editPersonalInformation() {
+  window.alert("hello world");
+}
+
+
+
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -36985,8 +36991,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Xamp\htdocs\web3\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Xamp\htdocs\web3\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\web3\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\web3\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/myProfile.blade.php
+++ resources/views/myProfile.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Personal Information</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,8 +14,13 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <p>Name: {{ Auth::user()->name }}</p>
+                    <p>Mail: {{ Auth::user()->email }}</p>
                 </div>
+
+                <button onclick="editPersonalInformation()">
+                    Edit personal information
+                </button>
             </div>
         </div>
     </div>
