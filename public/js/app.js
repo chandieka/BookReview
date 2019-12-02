20e5701dc79bd24bf7028f7ef78e6e9658111611
Hristov, Dimitar <mimaqm@gmail.com>
2019-12-02 10:14:22 +0100

    Revert "setup work environment"
    This reverts commit 7c3bd64f1fa470f4dc3db1471581e34408d277df.

--- public/js/app.js
+++ public/js/app.js
@@ -36985,8 +36985,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Documents\web3\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Documents\web3\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Xamp\htdocs\web3\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Xamp\htdocs\web3\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
