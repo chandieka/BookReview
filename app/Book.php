a5ddcc68a59dd4e00be9508d8db9e5c9669f34cc
chandieka <candiekatasbihyantra2@gmail.com>
2019-12-09 19:37:57 +0100

    Create The Reviews and Books relationship

--- app/Book.php
+++ app/Book.php
@@ -3,10 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     // option 1 
     protected $fillable = ['name', 'description', 'date'];
+
+    // RELATIONSHIP
+
+    public function reviews()
+    {
+        return $this->HasMany(Review::class);
+    }
 
 }
