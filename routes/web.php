43c93e1c4ea452bf5ffeeae311ad8a7d79654df7
Hristov, Dimitar <mimaqm@gmail.com>
2019-12-09 10:50:41 +0100

    auth added

--- routes/web.php
+++ routes/web.php
@@ -28,25 +28,25 @@ Route::get('/', function () {
 //
 //
 // route to the books page where all the books are shown
-Route::get('/books', 'BookController@index')->name('books.index');
+Route::get('/books', 'BookController@index')->name('books.index')->middleware('auth');
 
 // route to the book creation page
-Route::get('/books/create', 'BookController@create')->name('books.create');
+Route::get('/books/create', 'BookController@create')->name('books.create')->middleware('auth');
 
 // route the request to the controller
-Route::post('/books', 'BookController@store')->name('books.store');
+Route::post('/books', 'BookController@store')->name('books.store')->middleware('auth');
 
 // route to show a single book
 Route::get('/books/{book}', 'BookController@show')->name('books.show');
 
 // route to edit a book
-Route::get('/books/{book}/edit', 'BookController@edit')->name('books.edit');
+Route::get('/books/{book}/edit', 'BookController@edit')->name('books.edit')->middleware('auth');
 
 // route to update a book
-Route::patch('/books/{book}', 'BookController@update')->name('books.update');
+Route::patch('/books/{book}', 'BookController@update')->name('books.update')->middleware('auth');
 
 // route to destroy a book
-Route::delete('/books/{book}', 'BookController@destroy')->name('books.destroy');
+Route::delete('/books/{book}', 'BookController@destroy')->name('books.destroy')->middleware('auth');
 
 // PROFILE
 //
