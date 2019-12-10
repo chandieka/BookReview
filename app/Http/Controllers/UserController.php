9ad944536a5bd943d10056d5a08a411d424e60ca
Hildering,Raymond <i421449@student.fontys.nl>
2019-12-10 12:42:46 +0100

    update edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        $profile = \App\User::find($id);
+        $profile = Auth::user();
         return view('/profiles/edit', ['profile' => $profile]);
     }
 
