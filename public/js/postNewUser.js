b7c2811bd1a467c096c1ac76f97af87ee8a0a709
chandieka <candiekatasbihyantra2@gmail.com>
2020-01-07 17:02:34 +0100

    REST API Create request 1/2

---
+++ public/js/postNewUser.js
@@ -0,0 +1,36 @@
+document.getElementById('register').addEventListener('click',PostNewUser);
+
+let apiUrl = 'http://127.0.0.1:8000/api/user';
+
+
+async function PostNewUser() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let rePassword = document.getElementById('rePassword').value;
+
+    if (password === rePassword){
+
+        let newUser = {
+            name : name,
+            email : email,
+            password : password,
+        }
+
+        console.log(newUser);
+
+        // const response = await fetch(apiUrl, {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type' : 'application/json',
+        //     },
+        //     body: JSON.stringify(newUser),
+        // })
+
+        // const data = await response.json();
+
+        // console.log(data);
+    }
+
+}
