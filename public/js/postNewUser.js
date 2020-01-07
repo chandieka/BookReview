40af217fc5c140db3451a7cdfa4102f08b028040
chandieka tasbihyantra <45624070+chandieka@users.noreply.github.com>
2020-01-07 19:41:30 +0100

    Update postNewUser.js

--- public/js/postNewUser.js
+++ public/js/postNewUser.js
@@ -2,37 +2,47 @@ document.getElementById('register').addEventListener('click', PostNewUser);
 
 let apiUrl = 'http://127.0.0.1:8000/api/user';
 
-
 async function PostNewUser() {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let rePassword = document.getElementById('rePassword').value;
 
-    if (password === rePassword){
+    if (name !== '' && email !== '' && password !== '' && rePassword !== ''){
+        if (password === rePassword){
 
-        let newUser = {
-            name : name,
-            email : email,
-            password : password,
+            let newUser = {
+                name : name,
+                email : email,
+                password : password,
+            }
+
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify(newUser),
+            })
+            const data = await response.json();
         }
-
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(newUser),
-        })
-
-        const data = await response.json();
+        else {
+            // notify the user
+        }
+    }
+    else {
+        // notify the user
     }
 }
 
 function post() {
     PostNewUser()
     .then(
-        response => console.log(response)
-    )
+        response => {
+            console.log(response);
+    }).catch(
+        error => {
+            console.error(error);
+    })
 }
